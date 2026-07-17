4957a6f01e7a751bb653369fd087ae97dd32c95e
Qutiepatutie <ralphandre8@gmail.com>
2026-07-18 06:33:50 +0800

    Stage 2: read endpoints with 404

--- index.cjs
+++ index.cjs
@@ -2,6 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const tasks = [
+        {
+            id: 1,
+            title: "study",
+            done: false,
+        },
+        {
+            id: 2,
+            title: "work on project",
+            done: true,
+        },
+        {
+            id: 3,
+            title: "code",
+            done: false,
+        }
+]
+
 app.get('/', (req, res) => {
     res.send({
         name: "Task API",
@@ -13,8 +31,23 @@ app.get('/', (req, res) => {
 app.get('/health', (req, res) => {
     res.send({
         status: "ok"
-    })
-})
+    });
+});
+
+app.get('/tasks', (req, res) => {
+    res.send(tasks);
+});
+
+app.get('/tasks/:id', (req, res) => {
+
+    const task = tasks.find(item => item.id === Number(req.params.id));
+    
+    if (!task) {
+        return res.send({error: `Task ${req.params.id} was not found`});
+    }
+
+    res.send(task);
+});
 
 app.listen(port, () => {
     console.log(`App is listening to ${port}`);
